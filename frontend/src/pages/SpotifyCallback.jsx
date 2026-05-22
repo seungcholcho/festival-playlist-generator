b89ba53b2f2e93f6c627b5e031df9c847d8d34c1
조승철 <choseungchol@chol-2.local>
2026-05-22 19:31:30 +0900

    fix hardcoded localhost in callback pages

--- frontend/src/pages/SpotifyCallback.jsx
+++ frontend/src/pages/SpotifyCallback.jsx
@@ -1,28 +1,22 @@
 import { useEffect } from 'react'
 import axios from 'axios'
 
+const API_URL = import.meta.env.VITE_API_URL
+
 export function SpotifyCallback() {
   useEffect(() => {
     const params = new URLSearchParams(window.location.search)
     const code = params.get('code')
-
     if (code) {
-      axios.post('http://127.0.0.1:8000/api/spotify/token', { code })
+      axios.post(`${API_URL}/api/spotify/token`, { code })
         .then((res) => {
           const channel = new BroadcastChannel('spotify_auth')
-          channel.postMessage({
-            type: 'SPOTIFY_TOKEN',
-            access_token: res.data.access_token
-          })
+          channel.postMessage({ type: 'SPOTIFY_TOKEN', access_token: res.data.access_token })
           channel.close()
           setTimeout(() => window.close(), 500)
         })
-        .catch((err) => {
-          console.error(err)
-          window.close()
-        })
+        .catch((err) => { console.error(err); window.close() })
     }
   }, [])
-
   return <div>Spotify 로그인 중...</div>
 }
--- frontend/src/pages/YoutubeCallback.jsx
+++ frontend/src/pages/YoutubeCallback.jsx
@@ -1,28 +1,22 @@
 import { useEffect } from 'react'
 import axios from 'axios'
 
+const API_URL = import.meta.env.VITE_API_URL
+
 export function YoutubeCallback() {
   useEffect(() => {
     const params = new URLSearchParams(window.location.search)
     const code = params.get('code')
-
     if (code) {
-      axios.post('http://127.0.0.1:8000/api/youtube/token', { code })
+      axios.post(`${API_URL}/api/youtube/token`, { code })
         .then((res) => {
           const channel = new BroadcastChannel('youtube_auth')
-          channel.postMessage({
-            type: 'YOUTUBE_TOKEN',
-            access_token: res.data.access_token
-          })
+          channel.postMessage({ type: 'YOUTUBE_TOKEN', access_token: res.data.access_token })
           channel.close()
           setTimeout(() => window.close(), 500)
         })
-        .catch((err) => {
-          console.error(err)
-          window.close()
-        })
+        .catch((err) => { console.error(err); window.close() })
     }
   }, [])
-
   return <div>YouTube Music 로그인 중...</div>
 }
