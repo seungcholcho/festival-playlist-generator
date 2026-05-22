d6e00893b430291f3dd7edcdcc24345a344e48d7
조승철 <choseungchol@chol-2.local>
2026-05-22 19:11:15 +0900

    git cors rollback

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://festival-playlist-generator.vercel.app"
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
