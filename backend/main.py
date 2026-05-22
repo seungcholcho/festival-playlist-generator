6a3e73a63e7bcf33d59a6cbd7a41a60dec3cf6e1
조승철 <choseungchol@chol-2.local>
2026-05-22 19:08:20 +0900

    git cors wildcard

--- backend/main.py
+++ backend/main.py
@@ -6,10 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "https://festival-playlist-generator.vercel.app"],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
